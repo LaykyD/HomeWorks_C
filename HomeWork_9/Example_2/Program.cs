e76e6faf92897cd6f1ba655aa49b92f3c55aaab9
MosunovDA <layky-dima@mail.ru>
2023-02-14 00:01:45 +0300

    add hw_9 example_2

---
+++ HomeWork_9/Example_2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int InputIntNumber() {
+    int res;
+    while (!int.TryParse(Console.ReadLine(), out res))
+    {
+        Console.WriteLine("Введен неверный параметр! Введите целое число...");
+    }
+     return res;   
+}
+
+void SumNaturalNumbersRangeFromMToN(int m, int n, int sum){
+if (m > n) {
+    sum = 0;
+    Console.Write($"Сумма элементов = {sum} ");
+    return;
+}
+  sum = sum + n;
+  if (n <= m){
+    Console.Write($"Сумма элементов = {sum} ");
+    return;
+  }
+  SumNaturalNumbersRangeFromMToN(m, n - 1, sum);
+}
+
+Console.WriteLine("------------------------------------------------------------------------------ ");
+Console.WriteLine("Программа, которая нахолит сумму натуральных элементов в промежутке от M до N: ");
+Console.WriteLine("------------------------------------------------------------------------------ ");
+
+Console.Write("Введите число M: ");
+int numberM = InputIntNumber();
+Console.Write("Введите число N: ");
+int numberN = InputIntNumber();
+int sum = 0;
+
+SumNaturalNumbersRangeFromMToN(numberM, numberN, sum);
